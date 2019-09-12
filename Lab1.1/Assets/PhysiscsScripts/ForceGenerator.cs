a973bf84cd9d071eccbfd8c434b1e1728909c7c7
Dyson Little <dyson.little@mymail.champlain.edu>
2019-09-12 10:49:26 -0400

    Worked on force function definitions.

--- Lab1.1/Assets/PhysiscsScripts/ForceGenerator.cs
+++ Lab1.1/Assets/PhysiscsScripts/ForceGenerator.cs
@@ -12,22 +12,26 @@ public class ForceGenerator
 
     public static Vector2 GenerateForce_normal(Vector2 f_gravity, Vector2 surfaceNormal_unit)
     {
-        
+        Vector2 f_normal = (Vector2.Dot(f_gravity, surfaceNormal_unit) / Vector2.Dot(surfaceNormal_unit, surfaceNormal_unit)) * surfaceNormal_unit;
+        return f_normal;
     }
 
     public static Vector2 GenerateForce_sliding(Vector2 f_gravity, Vector2 f_normal)
     {
-
+        Vector2 f_sliding = f_gravity + f_normal;
+        return f_sliding;
     }
 
     public static Vector2 GenerateForce_friction_static(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_static)
     {
-
+        //make better
+        Vector2 f_friction_s = f_opposing.magnitude < (frictionCoefficient_static * f_normal).magnitude ? -1 * f_opposing : -1 * frictionCoefficient_static * f_normal;
+        return f_friction_s;
     }
 
     public static Vector2 GenerateForce_friction_kinetic(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_kinetic)
     {
-
+        Vector2 f_friction_k =
     }
 
     public static Vector2 GenerateForce_drag(Vector2 particleVelocity, Vector2 fluidVelocity, float fluidDensity, float objectArea_crossSection, float objectDragCoefficient)
